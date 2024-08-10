d88b04ecc7071813661fcd9ce715a0a35556e361
Rekha718 <162786383+Rekha718@users.noreply.github.com>
2024-08-10 09:03:00 +0530

    Update settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-4js+05asd)hzqqzy_2903qnm0+%sotq%de@cnyc@ccn_^w0!)e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
